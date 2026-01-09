507d4f7e5855d9be0734d4e84f86564e2e370523
Abdelrhman <abdelrhmanroshdy8@gmail.com>
2026-01-10 00:43:05 +0200

    equality operators

---
+++ Fundamentals/index.js
@@ -0,0 +1,12 @@
+const age = 18
+if (age === 18) console.log('you just became an adult');
+if (age === '18')
+  console.log('you just became an adult');
+else 
+  console.log('you are not adult yet');
+
+/**
+= assign value 
+== compare value  '18' == 18 -> true 
+=== compare value and data type  '18' === 18 -> false
+*/
